75ce595e5b8980b607f7a4d2a6bb84b8df92f9c7
Thomas Porzelt <tp@cryosphere.de>
2008-11-18 21:12:10 +0100

    add LIGHT_OS_XYZ macros

--- include/lighttpd/settings.h
+++ include/lighttpd/settings.h
@@ -1,6 +1,38 @@
 #ifndef _LIGHTTPD_SETTINGS_H_
 #define _LIGHTTPD_SETTINGS_H_
 
+/* check which OS we are compiling for */
+#if defined(__BEOS__)
+# define LIGHTY_OS_BEOS
+#elif (defined(__APPLE__) && defined(__GNUC__)) || defined(__MACOSX__)
+# define LIGHTY_OS_MACOSX
+#elif defined(__FreeBSD__)
+# define LIGHTY_OS_FREEBSD
+#elif defined(__NetBSD__)
+# define LIGHTY_OS_NETBSD
+#elif defined(__OpenBSD__)
+# define LIGHTY_OS_OPENBSD
+#elif defined(__sgi)
+# define LIGHTY_OS_IRIX
+#elif defined(__AIX)
+# define LIGHTY_OS_AIX
+#elif defined(__LINUX__) || defined(__linux__) || defined(__linux)
+# define LIGHTY_OS_LINUX
+#elif defined(__SUN__) || defined(__sun) || defined(sun)
+# define LIGHTY_OS_SOLARIS
+#elif defined(__hpux__) || defined(__hpux)
+# define LIGHTY_OS_HPUX
+#elif defined(WIN64) || defined(_WIN64) || defined(__WIN64__)
+# define LIGHTY_OS_WINDOWS
+# define LIGHTY_OS_WIN64
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+# define LIGHTY_OS_WINDOWS
+# define LIGHTY_OS_WIN32
+#else
+# warning "unknown OS, please report this"
+#endif
+
+
 #ifdef HAVE_CONFIG_H
 #include <lighttpd/config.h>
 #endif
