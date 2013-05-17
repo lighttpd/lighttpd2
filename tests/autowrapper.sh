de920597dca785015174122a18f13534db7282f9
Stefan Bühler <stbuehler@web.de>
2013-05-17 12:34:55 +0200

    [tests] extend autowrapper.sh script

--- tests/autowrapper.sh
+++ tests/autowrapper.sh
@@ -5,4 +5,4 @@ BUILD="$2"
 
 ARGS="${RUNTEST_ARGS}"
 
-exec "${TESTSRC}/runtests.py" --angel "${BUILD}/src/angel/lighttpd2" --worker "${BUILD}/src/main/lighttpd2-worker" --plugindir "${BUILD}/src/modules/.libs" $ARGS
+exec "${TESTSRC}/runtests.py" --angel "${BUILD}/src/angel/lighttpd2" --worker "${BUILD}/src/main/lighttpd2-worker" --plugindir "${BUILD}/src/modules/.libs" $ARGS "$@"
