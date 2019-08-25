bffe96f5d091685b4a8357f0547976a6ec71c4d6
Stefan Bühler <stbuehler@web.de>
2019-08-25 12:16:19 +0200

    [build] sync extra warning flags between cmake and autobuild
    Change-Id: I4165d22ef944b959f9acd8f57c3d7658049b39f0

--- cmake/LighttpdMacros.cmake
+++ cmake/LighttpdMacros.cmake
@@ -27,7 +27,7 @@ MACRO(ADD_TARGET_PROPERTIES _target _name)
 		SET(_properties "${_properties} ${_prop}")
 	ENDFOREACH(_prop)
 	GET_TARGET_PROPERTY(_old_properties ${_target} ${_name})
-	MESSAGE(STATUS "adding property to ${_target} ${_name}:" ${_properties})
+	#MESSAGE(STATUS "adding property to ${_target} ${_name}:" ${_properties})
 	IF(NOT _old_properties)
 		# in case it's NOTFOUND
 		SET(_old_properties)
