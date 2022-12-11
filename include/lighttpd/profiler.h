ecf68f11373afbd9db03b3fce748d4d291d0980f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-11 18:05:47 -0500

    spelling: deregisters
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- include/lighttpd/profiler.h
+++ include/lighttpd/profiler.h
@@ -21,6 +21,6 @@ LI_API void li_profiler_enable(gchar *output_path); /* enables the profiler */
 LI_API void li_profiler_finish(void);
 LI_API void li_profiler_dump(gint minsize); /* dumps memory statistics to file specified in LI_PROFILE_MEM env var */
 LI_API void li_profiler_hashtable_insert(const gpointer addr, gsize size); /* registers an allocated block with the profiler */
-LI_API void li_profiler_hashtable_remove(const gpointer addr); /* unregisters an allocated block with the profiler */
+LI_API void li_profiler_hashtable_remove(const gpointer addr); /* deregisters an allocated block with the profiler */
 
 #endif
